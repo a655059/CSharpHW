77f2377623f85d0fdfba9ba6cc83aa11aa23ac11
ShawnShen <a655059@hotmail.com.tw>
2022-06-16 14:11:35 +0800

    123

--- HW2/Loan.Designer.cs
+++ HW2/Loan.Designer.cs
@@ -29,20 +29,127 @@ namespace HW2
         /// </summary>
         private void InitializeComponent()
         {
+            this.labAmount = new System.Windows.Forms.Label();
+            this.labDue = new System.Windows.Forms.Label();
+            this.labRate = new System.Windows.Forms.Label();
+            this.labDown = new System.Windows.Forms.Label();
+            this.txtAmount = new System.Windows.Forms.TextBox();
+            this.txtDue = new System.Windows.Forms.TextBox();
+            this.txtRate = new System.Windows.Forms.TextBox();
+            this.txtDown = new System.Windows.Forms.TextBox();
+            this.btnPMT = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // labAmount
+            // 
+            this.labAmount.AutoSize = true;
+            this.labAmount.Location = new System.Drawing.Point(59, 47);
+            this.labAmount.Name = "labAmount";
+            this.labAmount.Size = new System.Drawing.Size(53, 12);
+            this.labAmount.TabIndex = 0;
+            this.labAmount.Text = "貸款金額";
+            // 
+            // labDue
+            // 
+            this.labDue.AutoSize = true;
+            this.labDue.Location = new System.Drawing.Point(59, 59);
+            this.labDue.Name = "labDue";
+            this.labDue.Size = new System.Drawing.Size(49, 12);
+            this.labDue.TabIndex = 1;
+            this.labDue.Text = "期限(年)";
+            // 
+            // labRate
+            // 
+            this.labRate.AutoSize = true;
+            this.labRate.Location = new System.Drawing.Point(59, 71);
+            this.labRate.Name = "labRate";
+            this.labRate.Size = new System.Drawing.Size(46, 12);
+            this.labRate.TabIndex = 2;
+            this.labRate.Text = "利率(%)";
+            // 
+            // labDown
+            // 
+            this.labDown.AutoSize = true;
+            this.labDown.Location = new System.Drawing.Point(59, 83);
+            this.labDown.Name = "labDown";
+            this.labDown.Size = new System.Drawing.Size(41, 12);
+            this.labDown.TabIndex = 3;
+            this.labDown.Text = "頭期款";
+            // 
+            // txtAmount
+            // 
+            this.txtAmount.Location = new System.Drawing.Point(149, 44);
+            this.txtAmount.Name = "txtAmount";
+            this.txtAmount.Size = new System.Drawing.Size(100, 22);
+            this.txtAmount.TabIndex = 4;
+            this.txtAmount.Text = "100000";
+            // 
+            // txtDue
+            // 
+            this.txtDue.Location = new System.Drawing.Point(149, 73);
+            this.txtDue.Name = "txtDue";
+            this.txtDue.Size = new System.Drawing.Size(100, 22);
+            this.txtDue.TabIndex = 5;
+            this.txtDue.Text = "2";
+            // 
+            // txtRate
+            // 
+            this.txtRate.Location = new System.Drawing.Point(149, 101);
+            this.txtRate.Name = "txtRate";
+            this.txtRate.Size = new System.Drawing.Size(100, 22);
+            this.txtRate.TabIndex = 6;
+            this.txtRate.Text = "10";
+            // 
+            // txtDown
+            // 
+            this.txtDown.Location = new System.Drawing.Point(149, 129);
+            this.txtDown.Name = "txtDown";
+            this.txtDown.Size = new System.Drawing.Size(100, 22);
+            this.txtDown.TabIndex = 7;
+            this.txtDown.Text = "0";
+            // 
+            // btnPMT
+            // 
+            this.btnPMT.Location = new System.Drawing.Point(61, 171);
+            this.btnPMT.Name = "btnPMT";
+            this.btnPMT.Size = new System.Drawing.Size(75, 23);
+            this.btnPMT.TabIndex = 8;
+            this.btnPMT.Text = "月付";
+            this.btnPMT.UseVisualStyleBackColor = true;
+            this.btnPMT.Click += new System.EventHandler(this.btnPMT_Click);
             // 
             // Loan
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnPMT);
+            this.Controls.Add(this.txtDown);
+            this.Controls.Add(this.txtRate);
+            this.Controls.Add(this.txtDue);
+            this.Controls.Add(this.txtAmount);
+            this.Controls.Add(this.labDown);
+            this.Controls.Add(this.labRate);
+            this.Controls.Add(this.labDue);
+            this.Controls.Add(this.labAmount);
             this.Name = "Loan";
             this.Text = "貸款金額計算";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labAmount;
+        private System.Windows.Forms.Label labDue;
+        private System.Windows.Forms.Label labRate;
+        private System.Windows.Forms.Label labDown;
+        private System.Windows.Forms.TextBox txtAmount;
+        private System.Windows.Forms.TextBox txtDue;
+        private System.Windows.Forms.TextBox txtRate;
+        private System.Windows.Forms.TextBox txtDown;
+        private System.Windows.Forms.Button btnPMT;
     }
 }
 
--- HW2/Loan.cs
+++ HW2/Loan.cs
@@ -16,5 +16,17 @@ namespace HW2
         {
             InitializeComponent();
         }
+
+        private void btnPMT_Click(object sender, EventArgs e)
+        {
+            int month = Int32.Parse(txtDue.Text) * 12;
+            double r = 0;
+            for(int i = 1; i <= month; i++)
+            {
+                r += Math.Pow(1.0/(1.0 + ((Double.Parse(txtRate.Text)/12.0)/100.0)),i);
+            }
+            double p = Double.Parse(txtAmount.Text) / r;
+            MessageBox.Show(p.ToString());
+        }
     }
 }
