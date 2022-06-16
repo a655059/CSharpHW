08280160dcb78f48e49cc9b01921739dfffceaf9
ShawnShen <a655059@hotmail.com.tw>
2022-06-16 16:38:58 +0800

    123131

---
+++ HW3/frm_BarPOS.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HW3
+{
+    public partial class frm_BarPOS : Form
+    {
+        public frm_BarPOS()
+        {
+            InitializeComponent();
+        }
+    }
+}
