f55ceb2a166956b391c8c0957e6c923cee97029d
ShawnShen <a655059@hotmail.com.tw>
2022-06-17 16:33:39 +0800

    123

---
+++ HW5/frm_StudentGrade.Designer.cs
@@ -0,0 +1,234 @@
+﻿
+namespace HW5
+{
+    partial class frm_StudentGrade
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtMath = new System.Windows.Forms.TextBox();
+            this.txtEnglish = new System.Windows.Forms.TextBox();
+            this.txtChinese = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.labMath = new System.Windows.Forms.Label();
+            this.labEnglish = new System.Windows.Forms.Label();
+            this.labChinese = new System.Windows.Forms.Label();
+            this.labName = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnRandomAdd = new System.Windows.Forms.Button();
+            this.btnStatistics = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnRandomAdd20 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // txtMath
+            // 
+            this.txtMath.Font = new System.Drawing.Font("新細明體", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txtMath.Location = new System.Drawing.Point(56, 96);
+            this.txtMath.Name = "txtMath";
+            this.txtMath.Size = new System.Drawing.Size(70, 25);
+            this.txtMath.TabIndex = 15;
+            this.txtMath.Text = "100";
+            // 
+            // txtEnglish
+            // 
+            this.txtEnglish.Font = new System.Drawing.Font("新細明體", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txtEnglish.Location = new System.Drawing.Point(56, 68);
+            this.txtEnglish.Name = "txtEnglish";
+            this.txtEnglish.Size = new System.Drawing.Size(70, 25);
+            this.txtEnglish.TabIndex = 14;
+            this.txtEnglish.Text = "100";
+            // 
+            // txtChinese
+            // 
+            this.txtChinese.Font = new System.Drawing.Font("新細明體", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txtChinese.Location = new System.Drawing.Point(56, 40);
+            this.txtChinese.Name = "txtChinese";
+            this.txtChinese.Size = new System.Drawing.Size(70, 25);
+            this.txtChinese.TabIndex = 13;
+            this.txtChinese.Text = "100";
+            // 
+            // txtName
+            // 
+            this.txtName.Font = new System.Drawing.Font("新細明體", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txtName.Location = new System.Drawing.Point(56, 12);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(70, 25);
+            this.txtName.TabIndex = 12;
+            this.txtName.Text = "王小明";
+            // 
+            // labMath
+            // 
+            this.labMath.AutoSize = true;
+            this.labMath.Font = new System.Drawing.Font("新細明體", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.labMath.Location = new System.Drawing.Point(15, 99);
+            this.labMath.Name = "labMath";
+            this.labMath.Size = new System.Drawing.Size(45, 15);
+            this.labMath.TabIndex = 11;
+            this.labMath.Text = "數學: ";
+            // 
+            // labEnglish
+            // 
+            this.labEnglish.AutoSize = true;
+            this.labEnglish.Font = new System.Drawing.Font("新細明體", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.labEnglish.Location = new System.Drawing.Point(15, 71);
+            this.labEnglish.Name = "labEnglish";
+            this.labEnglish.Size = new System.Drawing.Size(45, 15);
+            this.labEnglish.TabIndex = 10;
+            this.labEnglish.Text = "英文: ";
+            // 
+            // labChinese
+            // 
+            this.labChinese.AutoSize = true;
+            this.labChinese.Font = new System.Drawing.Font("新細明體", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.labChinese.Location = new System.Drawing.Point(15, 43);
+            this.labChinese.Name = "labChinese";
+            this.labChinese.Size = new System.Drawing.Size(45, 15);
+            this.labChinese.TabIndex = 9;
+            this.labChinese.Text = "國文: ";
+            // 
+            // labName
+            // 
+            this.labName.AutoSize = true;
+            this.labName.Font = new System.Drawing.Font("新細明體", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.labName.Location = new System.Drawing.Point(15, 15);
+            this.labName.Name = "labName";
+            this.labName.Size = new System.Drawing.Size(45, 15);
+            this.labName.TabIndex = 8;
+            this.labName.Text = "姓名: ";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(18, 127);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(108, 23);
+            this.btnAdd.TabIndex = 16;
+            this.btnAdd.Text = "加入學生資料";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnRandomAdd
+            // 
+            this.btnRandomAdd.Location = new System.Drawing.Point(18, 156);
+            this.btnRandomAdd.Name = "btnRandomAdd";
+            this.btnRandomAdd.Size = new System.Drawing.Size(108, 23);
+            this.btnRandomAdd.TabIndex = 17;
+            this.btnRandomAdd.Text = "隨機儲存資料";
+            this.btnRandomAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnStatistics
+            // 
+            this.btnStatistics.Enabled = false;
+            this.btnStatistics.Location = new System.Drawing.Point(18, 185);
+            this.btnStatistics.Name = "btnStatistics";
+            this.btnStatistics.Size = new System.Drawing.Size(108, 23);
+            this.btnStatistics.TabIndex = 18;
+            this.btnStatistics.Text = "各科統計";
+            this.btnStatistics.UseVisualStyleBackColor = true;
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(18, 249);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(108, 23);
+            this.btnReset.TabIndex = 19;
+            this.btnReset.Text = "重設所有資料";
+            this.btnReset.UseVisualStyleBackColor = true;
+            // 
+            // btnRandomAdd20
+            // 
+            this.btnRandomAdd20.Location = new System.Drawing.Point(18, 278);
+            this.btnRandomAdd20.Name = "btnRandomAdd20";
+            this.btnRandomAdd20.Size = new System.Drawing.Size(108, 23);
+            this.btnRandomAdd20.TabIndex = 20;
+            this.btnRandomAdd20.Text = "隨機加入20筆";
+            this.btnRandomAdd20.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(132, 214);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(369, 112);
+            this.textBox2.TabIndex = 22;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(132, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(369, 196);
+            this.richTextBox1.TabIndex = 23;
+            this.richTextBox1.Text = "";
+            // 
+            // frm_StudentGrade
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(511, 338);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.btnRandomAdd20);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnStatistics);
+            this.Controls.Add(this.btnRandomAdd);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtMath);
+            this.Controls.Add(this.txtEnglish);
+            this.Controls.Add(this.txtChinese);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.labMath);
+            this.Controls.Add(this.labEnglish);
+            this.Controls.Add(this.labChinese);
+            this.Controls.Add(this.labName);
+            this.Name = "frm_StudentGrade";
+            this.Text = "Myhomework";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtMath;
+        private System.Windows.Forms.TextBox txtEnglish;
+        private System.Windows.Forms.TextBox txtChinese;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label labMath;
+        private System.Windows.Forms.Label labEnglish;
+        private System.Windows.Forms.Label labChinese;
+        private System.Windows.Forms.Label labName;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnRandomAdd;
+        private System.Windows.Forms.Button btnStatistics;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnRandomAdd20;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ HW5/frm_StudentGrade.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HW5
+{
+    public partial class frm_StudentGrade : Form
+    {
+        public frm_StudentGrade()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
